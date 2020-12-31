efa6b3d8c1b6fcf35af88f0b99d98de45aa5e5ef
Mustapha <stoufa06@gmail.com>
2020-12-31 12:58:59 +0100

    Setup a skeleton of the library #1

---
+++ src/Training/Enumeration/Enumeration.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Garmin\Training\Enumeration;
+
+class Enumeration implements EnumerationInterface
+{
+
+    /**
+     * Default constructor
+     */
+    public function __construct()
+    {
+        // ...
+    }
+
+    /**
+     * 
+     */
+    public static function toString()
+    {
+        // TODO implement here
+    }
+
+}
+
+class Enumeration1
+{
+    const EnumerationLiteral1 = 0;
+}
---
+++ src/Training/Enumeration/equipmentType.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Garmin\Training\Enumeration;
+
+class equipmentType
+{
+    const NONE = 0;
+    const SWIM_FINS = 1;
+    const SWIM_KICKBOARD = 2;
+    const SWIM_PADDLES = 3;
+    const SWIM_PULL_BUOY = 4;
+    const SWIM_SNORKEL = 5;
+}
