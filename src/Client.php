0f93874444636337e0a245fa5efd8428130a54ac
Mustapha <stoufa06@gmail.com>
2021-01-05 22:33:16 +0100

    Implement the garmin connect Oauth 1 authorization #2

--- src/Client.php
+++ src/Client.php
@@ -4,121 +4,267 @@ declare(strict_types=1);
 
 namespace Garmin;
 
-class Client
+use DateTime;
+use DateTimeZone;
+use League\OAuth1\Client\Server\Server;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\GuzzleException;
+use InvalidArgumentException;
+use League\OAuth1\Client\Credentials\CredentialsException;
+use League\OAuth1\Client\Credentials\CredentialsInterface;
+use League\OAuth1\Client\Credentials\TemporaryCredentials;
+use League\OAuth1\Client\Credentials\TokenCredentials;
+use League\OAuth1\Client\Server\User;
+class Client extends Server
 {
 
-    public static const API_URL = '';
+    /**
+     * Api connect endpoint
+     */
+    public static const API_URL = ': https://connectapi.garmin.com/';
 
     /**
-     * Default constructor
+     * Rest api endpoint
      */
-    public function __construct()
+    const USER_API_URL = "https://apis.garmin.com/wellness-api/rest/";
+
+     /**
+     * Get the URL for retrieving temporary credentials.
+     *
+     * @return string
+     */
+    public function urlTemporaryCredentials(): string
     {
-        // ...
+        return self::API_URL . 'oauth-service/oauth/request_token';
     }
 
     /**
-     * 
+     * Get the URL for redirecting the resource owner to authorize the client.
+     *
+     * @return string
      */
-    public function urlTemporaryCredentials()
+    public function urlAuthorization(): string
     {
-        // TODO implement here
+        return 'http://connect.garmin.com/oauthConfirm';
     }
 
     /**
-     * 
+     * Get the URL retrieving token credentials.
+     *
+     * @return string
      */
-    public function urlAuthorization()
+    public function urlTokenCredentials(): string
     {
-        // TODO implement here
+        return self::API_URL . 'oauth-service/oauth/access_token';
     }
 
     /**
-     * 
+     * Get the authorization URL by passing in the temporary credentials
+     * identifier or an object instance.
+     *
+     * @param TemporaryCredentials|string $temporaryIdentifier
+     * @return string
      */
-    public function urlTokenCredentials()
+    public function getAuthorizationUrl($temporaryIdentifier): string
     {
-        // TODO implement here
+        // Somebody can pass through an instance of temporary
+        // credentials and we'll extract the identifier from there.
+        if ($temporaryIdentifier instanceof TemporaryCredentials) {
+            $temporaryIdentifier = $temporaryIdentifier->getIdentifier();
+        }
+        //$parameters = array('oauth_token' => $temporaryIdentifier, 'oauth_callback' => 'http://70.38.37.105:1225');
+
+        $url = $this->urlAuthorization();
+        //$queryString = http_build_query($parameters);
+        $queryString = "oauth_token=" . $temporaryIdentifier . "&oauth_callback=" . $this->clientCredentials->getCallbackUri();
+
+        return $this->buildUrl($url, $queryString);
     }
 
     /**
-     * 
+     * Retrieves token credentials by passing in the temporary credentials,
+     * the temporary credentials identifier as passed back by the server
+     * and finally the verifier code.
+     *
+     * @param TemporaryCredentials $temporaryCredentials
+     * @param string $temporaryIdentifier
+     * @param string $verifier
+     * @return TokenCredentials
+     * @throws CredentialsException If a "bad response" is received by the server
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
      */
-    public function getAuthorizationUrl()
+    public function getTokenCredentials(TemporaryCredentials $temporaryCredentials, string $temporaryIdentifier, string $verifier): TokenCredentials
     {
-        // TODO implement here
+        if ($temporaryIdentifier !== $temporaryCredentials->getIdentifier()) {
+            throw new \InvalidArgumentException(
+                'Temporary identifier passed back by server does not match that of stored temporary credentials.
+                Potential man-in-the-middle.'
+            );
+        }
+
+        $uri = $this->urlTokenCredentials();
+        $bodyParameters = array('oauth_verifier' => $verifier);
+
+        $client = $this->createHttpClient();
+
+        $headers = $this->getHeaders($temporaryCredentials, 'POST', $uri, $bodyParameters);
+        try {
+            $response = $client->post($uri, [
+                'headers' => $headers,
+                'form_params' => $bodyParameters
+            ]);
+        } catch (BadResponseException $e) {
+            throw $this->getCredentialsExceptionForBadResponse($e, 'token credentials');
+        }
+        
+        return $this->createTokenCredentials((string)$response->getBody());
     }
 
     /**
-     * 
+     * Get the base protocol parameters for an OAuth request.
+     * Each request builds on these parameters.
+     *
+     * @see OAuth 1.0 RFC 5849 Section 3.1
      */
-    public function getTokenCredentials()
+    protected function baseProtocolParameters(): array
     {
-        // TODO implement here
+        $dateTime = new DateTime('now', new DateTimeZone('UTC'));
+
+        return [
+            'oauth_consumer_key' => $this->clientCredentials->getIdentifier(),
+            'oauth_nonce' => $this->nonce(),
+            'oauth_signature_method' => $this->signature->method(),
+            'oauth_timestamp' => $dateTime->format('U'),
+            'oauth_version' => '1.0',
+        ];
+    }
+
+   /**
+     * Generate the OAuth protocol header for requests other than temporary
+     * credentials, based on the URI, method, given credentials & body query
+     * string.
+     * 
+     * @param string $method
+     * @param string $uri
+     * @param CredentialsInterface $credentials
+     * @param array $bodyParameters
+     * @return string
+     */
+    protected function protocolHeader(string $method, string $uri, CredentialsInterface $credentials, array $bodyParameters = array()): string
+    {
+        $parameters = array_merge(
+            $this->baseProtocolParameters(),
+            $this->additionalProtocolParameters(),
+            array(
+                'oauth_token' => $credentials->getIdentifier(),
+
+            ),
+            $bodyParameters
+        );
+        $this->signature->setCredentials($credentials);
+
+        $parameters['oauth_signature'] = $this->signature->sign(
+            $uri,
+            array_merge($parameters, $bodyParameters),
+            $method
+        );
+
+        return $this->normalizeProtocolParameters($parameters);
+    }
+
+
+    /**
+     * delete user access token: deregistration
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @return void
+     * @throws Exception
+     */
+    public function deleteUserAccessToken(TokenCredentials $tokenCredentials): void
+    {
+        $uri = 'user/registration';
+        $client = $this->createHttpClient();
+        $headers = $this->getHeaders($tokenCredentials, 'DELETE', self::USER_API_URL . $uri);
+
+        try {
+            $response = $client->delete(self::USER_API_URL . $uri, [
+                'headers' => $headers,
+            ]);
+        } catch (BadResponseException $e) {
+            $response = $e->getResponse();
+            $body = $response->getBody();
+            $statusCode = $response->getStatusCode();
+
+            throw new \Exception(
+                "Received error [$body] with status code [$statusCode] when deleting user access token."
+            );
+        }
     }
 
     /**
-     * 
+     * returns user details url
+     *
+     * @return string
      */
-    public function baseProtocolParameters()
+    public function urlUserDetails(): string
     {
-        // TODO implement here
+        return self::USER_API_URL . 'user/id';
     }
 
     /**
-     * 
+     * get user details: in garmin there is only user id
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     * @return User
      */
-    public function protocolHeader()
+    public function userDetails($data, TokenCredentials $tokenCredentials): User
     {
-        // TODO implement here
+        $user = new User();
+
+        $user->uid = $data['userId'];
+
+
+        $user->extra = (array) $data;
+
+        return $user;
     }
 
     /**
-     * 
+     * get user id
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     *  @return string|int|null
      */
-    public function deleteUserAccessToken()
+    public function userUid($data, TokenCredentials $tokenCredentials)
     {
-        // TODO implement here
+        return isset($data['userId']) ? $data['userId'] : null;
     }
 
     /**
-     * 
+     * Left for compatibilty
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     * @return string return empty string
      */
-    public function urlUserDetails()
+    public function userEmail($data, TokenCredentials $tokenCredentials): string
     {
-        // TODO implement here
+        return '';
     }
 
     /**
-     * 
+     * Left for compatibilty
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     * @return string return empty string
      */
-    public function userDetails()
+    public function userScreenName($data, TokenCredentials $tokenCredentials): string
     {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public function userUid()
-    {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public function userEmail()
-    {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public function userScreenName()
-    {
-        // TODO implement here
+        return '';
     }
 
 }
